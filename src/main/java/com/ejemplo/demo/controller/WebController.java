57c87a5567152d396cf3024954eb17a528f00896
Carlitos <Carlitos@DESKTOP-9611RRB>
2022-04-19 20:08:34 -0600

    EXAMEN 2

---
+++ src/main/java/com/ejemplo/demo/controller/WebController.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ejemplo.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ *
+ * @author Carlitos
+ */
+@Controller
+public class WebController {
+        @GetMapping({"/"})
+    public String index(){
+        return "ejemplo";
+    }
+}
